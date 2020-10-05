576c738daf7b875b761c35f2975b089205f83aa1
Ivan <studentik7@yandex.ru>
2020-10-05 13:45:20 +0300

    - refactored OrderRepositoryImpl

--- src/main/java/ru/spbstu/hsisct/stockmarket/repository/OrderRepositoryImpl.java
+++ src/main/java/ru/spbstu/hsisct/stockmarket/repository/OrderRepositoryImpl.java
@@ -2,23 +2,18 @@ package ru.spbstu.hsisct.stockmarket.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-import ru.spbstu.hsisct.stockmarket.model.Broker;
-import ru.spbstu.hsisct.stockmarket.model.Company;
-import ru.spbstu.hsisct.stockmarket.model.Individual;
 import ru.spbstu.hsisct.stockmarket.model.Order;
 import ru.spbstu.hsisct.stockmarket.model.enums.OrderOperationType;
 import ru.spbstu.hsisct.stockmarket.model.enums.OrderStatus;
 
 import java.sql.PreparedStatement;
-import java.sql.SQLDataException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
-import java.util.NoSuchElementException;
 import java.util.Objects;
 
 @Repository
@@ -56,21 +51,10 @@ public class OrderRepositoryImpl implements OrderRepository {
                         INSERT INTO "order" (broker_id, company_id, individual_id, size, min_price, max_price, operation_type, order_status, timestamp, parent_id) 
                         VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?); 
                     """, Statement.RETURN_GENERATED_KEYS);
-                    statement.setLong(1, broker_id);
-                    statement.setLong(2, company_id);
-                    statement.setLong(3, individual_id);
-                    statement.setLong(4, order.getSize());
-                    statement.setObject(5, order.isLimitedOrder() ? order.getMinPrice() : null);
-                    statement.setObject(6, order.isLimitedOrder() ? order.getMaxPrice() : null);
-                    statement.setString(7, order.getOperationType().name());
-                    statement.setString(8, order.getOrderStatus().name());
-                    statement.setObject(9, order.getTimestamp());
-                    statement.setObject(10, order.getParentId()); //TODO check that Long unboxes and null is also supported
+                    injectOrderFieldsIntoStatement(order, broker_id, company_id, individual_id, statement);
 
                     return statement;
-                },
-                keyHolder
-        );
+                }, keyHolder);
 
         final long id = (long) Objects.requireNonNull(keyHolder.getKeys()).get("id");
         return Objects.requireNonNull(jdbcTemplate.query(
@@ -81,24 +65,7 @@ public class OrderRepositoryImpl implements OrderRepository {
                     statement.setLong(1, id);
 
                     return statement;
-                },
-                rs -> {
-                    if (!rs.next()) {
-                        throw new RuntimeException("TODO"); //TODO implement
-                    }
-
-                    return Order.builder()
-                            .id(rs.getLong("id"))
-                            .size(rs.getLong("size"))
-                            .minPrice(rs.getBigDecimal("min_price"))
-                            .maxPrice(rs.getBigDecimal("max_price"))
-                            .operationType(OrderOperationType.valueOf(rs.getString("operation_type")))
-                            .orderStatus(OrderStatus.valueOf(rs.getString("order_status")))
-                            .isPublic(rs.getBoolean("public"))
-                            .timestamp(rs.getObject("timestamp", LocalDateTime.class))
-                            .parentId(rs.getLong("parent_id"))
-                            .build();
-                })
+                }, OrderRepositoryImpl::constructOrderFromResultSet)
         );
     }
 
@@ -107,11 +74,11 @@ public class OrderRepositoryImpl implements OrderRepository {
         return null;
     }
 
-    private static PreparedStatement injectOrderIntoStatement(final Order order,
-                                                              final long broker_id,
-                                                              final long company_id,
-                                                              final long individual_id,
-                                                              final PreparedStatement statement) throws SQLException {
+    private static void injectOrderFieldsIntoStatement(final Order order,
+                                                       final long broker_id,
+                                                       final long company_id,
+                                                       final long individual_id,
+                                                       final PreparedStatement statement) throws SQLException {
         statement.setLong(1, broker_id);
         statement.setLong(2, company_id);
         statement.setLong(3, individual_id);
@@ -122,7 +89,24 @@ public class OrderRepositoryImpl implements OrderRepository {
         statement.setString(8, order.getOrderStatus().name());
         statement.setObject(9, order.getTimestamp());
         statement.setObject(10, order.getParentId());
-
-        return statement;
     }
+
+    private static Order constructOrderFromResultSet(final ResultSet rs) throws SQLException {
+        if (!rs.next()) {
+            throw new RuntimeException("TODO"); //TODO implement
+        }
+
+        return Order.builder()
+                .id(rs.getLong("id"))
+                .size(rs.getLong("size"))
+                .minPrice(rs.getBigDecimal("min_price"))
+                .maxPrice(rs.getBigDecimal("max_price"))
+                .operationType(OrderOperationType.valueOf(rs.getString("operation_type")))
+                .orderStatus(OrderStatus.valueOf(rs.getString("order_status")))
+                .isPublic(rs.getBoolean("public"))
+                .timestamp(rs.getObject("timestamp", LocalDateTime.class))
+                .parentId(rs.getLong("parent_id"))
+                .build();
+    }
+
 }
