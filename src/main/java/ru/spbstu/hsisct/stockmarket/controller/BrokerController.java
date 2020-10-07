a58b2489d9034d9d4e3cb6a27e60b6865b2bf458
Ivan <studentik7@yandex.ru>
2020-10-07 20:48:07 +0300

    - broker new account page ready

--- src/main/java/ru/spbstu/hsisct/stockmarket/controller/BrokerController.java
+++ src/main/java/ru/spbstu/hsisct/stockmarket/controller/BrokerController.java
@@ -1,17 +1,43 @@
 package ru.spbstu.hsisct.stockmarket.controller;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.spbstu.hsisct.stockmarket.model.Broker;
+import ru.spbstu.hsisct.stockmarket.repository.BrokerRepository;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping(value = "/broker")
 public class BrokerController {
 
+    private final BrokerRepository brokerRepository;
+
     @GetMapping("/new-broker")
-    public String createNewBroker() {
+    public String createNewBroker(Model model) {
+        model.addAttribute("Broker", new Broker());
+
         return "broker/new-broker";
+    }
+
+    @PostMapping(value = "/add-broker", consumes = "application/x-www-form-urlencoded")
+    public String addNewBroker(Broker broker) {
+        var brokerEntity = brokerRepository.save(broker);
+
+        return "redirect:/broker/lk/" + brokerEntity.getId();
+    }
+
+    @GetMapping("/lk/{brokerId}")
+    public String getBrokerHomePage(@PathVariable("brokerId") @NonNull Long userId) {
+        return "broker/lk";
     }
 }
--- src/main/java/ru/spbstu/hsisct/stockmarket/controller/IndividualController.java
+++ src/main/java/ru/spbstu/hsisct/stockmarket/controller/IndividualController.java
@@ -5,27 +5,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.spbstu.hsisct.stockmarket.dto.OrderDto;
 import ru.spbstu.hsisct.stockmarket.facade.BrokerFacade;
 import ru.spbstu.hsisct.stockmarket.facade.IndividualFacade;
-import ru.spbstu.hsisct.stockmarket.model.Broker;
 import ru.spbstu.hsisct.stockmarket.model.Individual;
-import ru.spbstu.hsisct.stockmarket.repository.BrokerRepository;
 import ru.spbstu.hsisct.stockmarket.repository.CompanyRepository;
 import ru.spbstu.hsisct.stockmarket.repository.IndividualRepository;
 import ru.spbstu.hsisct.stockmarket.repository.StockRepository;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @Controller
@@ -47,8 +41,8 @@ public class IndividualController {
         return "user/new-user";
     }
 
-    @PostMapping("/add-user")
-    public String addNewUser(@ModelAttribute("individual") @RequestBody Individual user) {
+    @PostMapping(value = "/add-user", consumes = "application/x-www-form-urlencoded")
+    public String addNewUser(Individual user) {
         var individual = individualRepository.save(user);
 
         return "redirect:/user/lk/" + individual.getId();
@@ -90,6 +84,7 @@ public class IndividualController {
         return "redirect:/user/lk/" + userId;
     }
 
+    @SuppressWarnings("SpringMVCViewInspection")
     @PostMapping("lk/{userId}/delete-account")
     public String deleteAccount(@PathVariable("userId") @NonNull Long userId) {
         log.info(userId.toString());
